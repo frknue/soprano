beac85a41b17d0141fdb87b0c5ff90e7d0c589a5
frknue <furkan.uelker@hotmail.com>
2026-07-23 16:32:30 +0200

    Fix sided modifier release transitions

--- Tests/SopranoTests/TerminalInputMetadataTests.swift
+++ Tests/SopranoTests/TerminalInputMetadataTests.swift
@@ -38,42 +38,98 @@ struct TerminalInputMetadataTests {
     @Test func leftModifierTransitionsReportPressAndRelease() {
         #expect(TerminalInputMetadata.modifierTransition(
             keyCode: 0x38,
-            modifiers: [.shift]
+            modifiers: [.shift],
+            deviceModifiers: [.shiftLeft]
         ) == .press)
         #expect(TerminalInputMetadata.modifierTransition(
             keyCode: 0x38,
-            modifiers: []
+            modifiers: [],
+            deviceModifiers: []
         ) == .release)
     }
 
     @Test func rightModifierTransitionsUseTheRightSideState() {
         #expect(TerminalInputMetadata.modifierTransition(
             keyCode: 0x3D,
-            modifiers: [.option, .optionRight]
+            modifiers: [.option, .optionRight],
+            deviceModifiers: [.optionRight]
         ) == .press)
         #expect(TerminalInputMetadata.modifierTransition(
             keyCode: 0x3D,
-            modifiers: [.option]
+            modifiers: [.option],
+            deviceModifiers: [.optionLeft]
         ) == .release)
     }
 
     @Test func everySupportedRightModifierReportsPressAndRelease() {
-        let cases: [(UInt16, TerminalModifierFlags, TerminalModifierFlags)] = [
-            (0x3C, [.shift, .shiftRight], [.shift]),
-            (0x3E, [.control, .controlRight], [.control]),
-            (0x3D, [.option, .optionRight], [.option]),
-            (0x36, [.command, .commandRight], [.command]),
+        let cases: [
+            (
+                keyCode: UInt16,
+                pressedModifiers: TerminalModifierFlags,
+                releasedModifiers: TerminalModifierFlags,
+                right: TerminalModifierDeviceFlags,
+                left: TerminalModifierDeviceFlags
+            )
+        ] = [
+            (0x3C, [.shift, .shiftRight], [.shift], .shiftRight, .shiftLeft),
+            (0x3E, [.control, .controlRight], [.control], .controlRight, .controlLeft),
+            (0x3D, [.option, .optionRight], [.option], .optionRight, .optionLeft),
+            (0x36, [.command, .commandRight], [.command], .commandRight, .commandLeft),
         ]
 
-        for (keyCode, pressed, released) in cases {
+        for testCase in cases {
             #expect(TerminalInputMetadata.modifierTransition(
-                keyCode: keyCode,
-                modifiers: pressed
+                keyCode: testCase.keyCode,
+                modifiers: testCase.pressedModifiers,
+                deviceModifiers: testCase.right
             ) == .press)
             #expect(TerminalInputMetadata.modifierTransition(
-                keyCode: keyCode,
-                modifiers: released
+                keyCode: testCase.keyCode,
+                modifiers: testCase.releasedModifiers,
+                deviceModifiers: testCase.left
             ) == .release)
         }
+    }
+
+    @Test func releasingLeftModifierWhileRightRemainsHeldReportsRelease() {
+        let cases: [
+            (
+                keyCode: UInt16,
+                modifiers: TerminalModifierFlags,
+                left: TerminalModifierDeviceFlags,
+                right: TerminalModifierDeviceFlags
+            )
+        ] = [
+            (0x38, [.shift, .shiftRight], .shiftLeft, .shiftRight),
+            (0x3B, [.control, .controlRight], .controlLeft, .controlRight),
+            (0x3A, [.option, .optionRight], .optionLeft, .optionRight),
+            (0x37, [.command, .commandRight], .commandLeft, .commandRight),
+        ]
+
+        for testCase in cases {
+            #expect(TerminalInputMetadata.modifierTransition(
+                keyCode: testCase.keyCode,
+                modifiers: testCase.modifiers,
+                deviceModifiers: testCase.left
+            ) == .press)
+            #expect(TerminalInputMetadata.modifierTransition(
+                keyCode: testCase.keyCode,
+                modifiers: testCase.modifiers,
+                deviceModifiers: testCase.right
+            ) == .release)
+        }
+    }
+
+    @Test func capsLockTransitionUsesItsAggregateToggleState() {
+        #expect(TerminalInputMetadata.modifierTransition(
+            keyCode: 0x39,
+            modifiers: [.capsLock],
+            deviceModifiers: []
+        ) == .press)
+        #expect(TerminalInputMetadata.modifierTransition(
+            keyCode: 0x39,
+            modifiers: [],
+            deviceModifiers: []
+        ) == .release)
     }
 }
